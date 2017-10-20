a6ead4646120cb04ae6ca115416b62ea07284b71
Adham Kurniawan <dmkrnwn@gmail.com>
2017-10-21 02:23:51 +0700

    added styling to the navigation bar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,17 +5,18 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    @post.user = current_user.id
+    @post.user_id = current_user.id
     respond_to do |f|
       if (@post.save)
-        f.html { redirect_to: "", notice: "Post created!" }
+        f.html { redirect_to "", notice: "Post created!" }
       else
-        f.html { redirect_to: "", notice: "Post not saved."}
+        f.html { redirect_to "", notice: "Post not saved."}
+      end
     end
   end
 
   private
     def post_params
-      params.reuquire(:post).permit(:user_id, :content)
+      params.require(:post).permit(:user_id, :content)
     end
 end
